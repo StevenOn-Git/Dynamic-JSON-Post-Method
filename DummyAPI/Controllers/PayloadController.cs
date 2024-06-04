607492edf3526705163d3cd20f2efa1797dd6302
steveonsmud <steven.on@smud.org>
2024-06-04 14:20:12 -0700

    Add project files.

---
+++ DummyAPI/Controllers/PayloadController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using DummyAPI.Utilitites;
+
+namespace DummyAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayloadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult ReceivePayload([FromBody] JsonElement payload)
+        {
+            if (payload.ValueKind == JsonValueKind.Undefined)
+            {
+                return BadRequest("Payload is required");
+            }
+
+            string payloadString = JsonSerializer.Serialize(payload);
+            // Logs are on the QA MQ 01 server on the D drive. 
+            LogWriter.Log($"JSONPayload: {payloadString}");
+
+            return Ok(new 
+            { 
+                Message = "Payload received successfully",
+                PayloadString = payloadString
+            });
+        }
+    }
+}
---
+++ DummyAPI/Utilitites/LogWriter.cs
@@ -0,0 +1,47 @@
+﻿namespace DummyAPI.Utilitites
+{
+    public class LogWriter
+    {
+        public static void Log(string logMessage)
+        {
+            DateTime currentDate = DateTime.Now;
+            string dateFormat = "yyyyMMdd";
+            string filePath = "D:\\DummyAPILogs\\";
+            string nameFile = "DummyAPILog_";
+            string fileExt = ".log";
+
+            string fileName = filePath + nameFile + currentDate.ToString(dateFormat) + fileExt;
+
+            try
+            {
+                // File Creation
+                if (!File.Exists(fileName))
+                {
+                    using (StreamWriter sw = File.CreateText(fileName))
+                    {
+                        sw.WriteLine("DummyAPILogger");
+                        sw.Dispose();
+                        sw.Close();
+                    }
+                }
+
+                using (StreamWriter sw = File.AppendText(fileName))
+                {
+                    sw.Write($"{DateTime.Now.ToString()}");
+                    sw.WriteLine($"| {logMessage}");
+                    sw.Dispose();
+                    sw.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                using (StreamWriter sw = File.CreateText(fileName))
+                {
+                    sw.WriteLine("**********************************");
+                    sw.WriteLine(ex.ToString());
+                    sw.WriteLine("**********************************");
+                }
+            }
+        }
+    }
+}
